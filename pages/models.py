567073f778db045f58be153ef3794293a2f6c4ea
JassCoder <development.jass@gmail.com>
2021-04-18 17:11:15 -0500

    Cambio field content por nombre2

--- pages/models.py
+++ pages/models.py
@@ -3,7 +3,7 @@ from ckeditor.fields import RichTextField
 
 class Page(models.Model):
     nombre1 = models.CharField(verbose_name="Primer Nombre", max_length=200)
-    nombre2 = RichTextField(verbose_name="Segundo Nombre")
+    nombre2 = models.CharField(verbose_name="Segundo Nombre")
     order = models.SmallIntegerField(verbose_name="Orden", default=0)
     created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
